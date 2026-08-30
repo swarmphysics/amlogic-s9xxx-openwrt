1587aafb144cc3937a51a125231058dc91f0aa32
Zhanwei Gao <46527963+swarmphysics@users.noreply.github.com>
2026-08-30 23:26:26 +0800

    Add feed sources for istore and nas-packages
    Add feed sources for istore and nas-packages to feeds.conf.default

--- config/openwrt_main/diy-part1.sh
+++ config/openwrt_main/diy-part1.sh
@@ -12,3 +12,9 @@
 # Remove unnecessary packages
 # rm -rf package/utils/{ucode,fbtest}
 
+# add feed sources for istore & nas-packages
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
